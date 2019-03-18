d416d49acb4945456823e613098830b45f237eb9
CATHERINE MENG <catherine@CATHERINEs-MBP.hitronhub.home>
2019-03-18 16:15:39 -0700

    udpate the format of wageData and pasted it into index.js

--- data/readData.py
+++ data/readData.py
@@ -7,19 +7,30 @@ map_industry = {'Goods producing industries 7': 'Goods Producting',
 'Service producing industries 9': 'Service', 
 'Transportation and warehousing': 'Transportation',
 'Finance and insurance': 'Finance', 
-'Professional, scientific and technical services': 'Technical Services', 
+'Professional scientific and technical services': 'Technical Services', 
 'Educational services': 'Education', 
 'Health care and social assistance': 'Health Care',
-'Arts, entertainment and recreation': 'Arts'}
+'Arts entertainment and recreation': 'Arts'}
 
 provinces = ['Newfoundland and Labrador', 'Prince Edward Island', 'Nova Scotia', 'New Brunswick', 
 'Quebec', 'Ontario', 'Manitoba', 'Saskatchewan', 'Alberta', 'British Columbia']
-years=[2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
-data = {2008:{}, 2009:{}, 2010:{}, 2011: {}, 2012: {},
-2013: {}, 2014: {}, 2015: {}, 2016: {}, 2017:{}}
-for year in data:
-	for province in provinces:
-		data[year][province] = {}
+map_provinces = {'Newfoundland and Labrador': 'NL', 'Prince Edward Island': 'PE', 'Nova Scotia': 'NS', 'New Brunswick': 'NB', 
+'Quebec': 'QC', 'Ontario': 'OT', 'Manitoba': 'MB', 'Saskatchewan': 'SK', 'Alberta': 'AB', 'British Columbia': 'BC'}
+# years=[2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
+data = {'Newfoundland and Labrador':{}, 
+'Prince Edward Island':{}, 
+'Nova Scotia':{}, 
+'New Brunswick': {}, 
+'Quebec': {},
+'Ontario': {}, 
+'Manitoba': {}, 
+'Saskatchewan': {}, 
+'Alberta': {}, 
+'British Columbia':{}}
+
+for province in data:
+	for industry in map_industry:
+		data[province][map_industry[industry]] = []
 
 for line in lines:
 	linelist = line.split(',')
@@ -27,35 +38,38 @@ for line in lines:
 		curInd = linelist[0].replace('"', '')
 		if curInd in map_industry:
 			values = linelist[1:]
+			industry = map_industry[curInd]
 			for j in range(10):
+				province = provinces[j]
 				for i in range(10):
 					index = i + j * 10
-					province = provinces[j]
-					year = years[i]
-					industry = map_industry[curInd]
-					data[year][province][industry] = values[index]
+					data[province][industry].append(values[index].replace('"', ''))
 
-#print(data)
-
+# print(data)
+formatted_data = {}
+for province in data:
+	formatted_data[province] = []
+	for industry in data[province]:
+		indList = []
+		indList.append(str(map_provinces[province] + '-' + industry))
+		indList.append(0)
+		for value in data[province][industry]:
+			if value == '..' or value == 'F' or value == 'x':
+				indList.append(0)
+			else:
+				indList.append(float(value))
+		formatted_data[province].append(indList)
+print(formatted_data)
 
 # data format for wage rate will be like:
-# {'2008': {
-# 	'AB':{
-# 		'Goods Producing':33.04,
-# 		'Construction':28.63,
+# {'AB':{
+# 		'Goods Producing':[33.04, 32.06 .....] # for 10 years
+# 		'Construction': [28.63....],
 # 	},
-# 	'BC':{
-# 		'Goods Producing':33.04,
-# 		'Construction':28.63,
+# 'BC':{
+# 		'Goods Producing':[33.04...],
+# 		'Construction':[28.63...],
 # 	}
-# },
-# '2009':{
-#	'AB':{
-# 		'Goods Producing':33.04,
-# 		'Construction':28.63,
-# 	},
-# 	'BC':{
-# 		'Goods Producing':33.04,
-# 		'Construction':28.63,
-# 	}
+# }
+
 #}}
